845cf14f3f608b4212e73ac0151157831ccd6e39
gulten27 <ggdogann00@gmail.com>
2023-05-05 14:51:20 +0300

    navigation compose

--- app/src/main/java/com/example/cryptocompose/MainActivity.kt
+++ app/src/main/java/com/example/cryptocompose/MainActivity.kt
@@ -8,9 +8,17 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavType
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import androidx.navigation.navArgument
 import com.example.cryptocompose.ui.theme.CryptoComposeTheme
+import com.example.cryptocompose.view.CryptoDetailScreen
+import com.example.cryptocompose.view.CryptoListScreen
 
 
 class MainActivity : ComponentActivity() {
@@ -18,30 +26,36 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CryptoComposeTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = "crypto_list_screen"){
+                    composable("crypto_list_screen"){
+                        CryptoListScreen(navController = navController)
+                    }
+
+                    composable("crypto_detail_screen/{cryptoId}/{cryptoPrice}", arguments = listOf(
+                        navArgument("cryptoId"){
+                            type = NavType.StringType
+                        },
+                        navArgument("cryptoPrice"){
+                            type = NavType.StringType
+                        }
+                    )){
+                        val cryptoId = remember{
+                            it.arguments?.getString("cryptoId")
+                        }
+
+                        val cryptoPrice = remember{
+                            it.arguments?.getString("cryptoPrice")
+                        }
+
+                        CryptoDetailScreen(
+                            id = cryptoId ?: "",
+                            price = cryptoPrice ?: "",
+                            navController = navController)
+                    }
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    CryptoComposeTheme {
-        Greeting("Android")
-    }
-}
