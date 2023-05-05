c4e52f4fce4cf52346dfed2c999541777d4bf9e2
gulten27 <ggdogann00@gmail.com>
2023-05-05 15:53:39 +0300

    repository

---
+++ app/src/main/java/com/example/cryptocompose/repository/CryptoRepository.kt
@@ -0,0 +1,36 @@
+package com.example.cryptocompose.repository
+
+import com.example.cryptocompose.model.Crypto
+import com.example.cryptocompose.model.CryptoList
+import com.example.cryptocompose.service.CryptoAPI
+import com.example.cryptocompose.util.Constants.API_KEY
+import com.example.cryptocompose.util.Constants.CALL_ATTRIBUTES
+import com.example.cryptocompose.util.Resource
+import dagger.hilt.android.scopes.ActivityScoped
+import javax.inject.Inject
+import kotlin.contracts.Returns
+
+@ActivityScoped
+class CryptoRepository @Inject constructor(
+    private val api : CryptoAPI
+){
+
+    suspend fun getCryptoList() : Resource<CryptoList>{
+        val response = try {
+            api.getCryptoList(API_KEY)
+        }catch (e:Exception){
+            return Resource.Error("Error")
+        }
+
+        return Resource.Success(response)
+    }
+
+    suspend fun getCrypto(id:String) : Resource<Crypto> {
+        val response = try {
+            api.getCrypto(API_KEY,id, CALL_ATTRIBUTES)
+        }catch (e : Exception){
+            return Resource.Error("Error")
+        }
+        return Resource.Success(response)
+    }
+}
--- app/src/main/java/com/example/cryptocompose/service/CryptoAPI.kt
+++ app/src/main/java/com/example/cryptocompose/service/CryptoAPI.kt
@@ -1,5 +1,7 @@
 package com.example.cryptocompose.service
 
+import com.example.cryptocompose.model.Crypto
+import com.example.cryptocompose.model.CryptoList
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -8,12 +10,12 @@ interface CryptoAPI {
     @GET("prices")
     suspend fun getCryptoList(
         @Query("key") key : String
-    ): Unit
+    ): CryptoList
 
     @GET("currencies")
     suspend fun getCrypto(
         @Query("key") key: String,
         @Query("ids") id: String,
         @Query("attributes") attributes: String
-    ): Unit
+    ): Crypto
 }
